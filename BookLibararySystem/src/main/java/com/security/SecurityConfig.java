d362a75d716449fb1f1ed7fdc50614dc274dabc9
Noorish100 <mdnoorishnauman@gmail.com>
2024-06-04 15:04:35 +0530

    bassic security

--- BookLibararySystem/src/main/java/com/security/SecurityConfig.java
+++ BookLibararySystem/src/main/java/com/security/SecurityConfig.java
@@ -1,26 +1,27 @@
-//package com.security;
-//
-//import java.beans.JavaBean;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
-//
-//@Configuration
-//@EnableWebSecurity
-//@JavaBean
-//public class SecurityConfig extends WebSecurityConfiguration {
-//
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http
-//            .csrf().disable() // Disable CSRF
-//            .cors()            // Enable CORS
-//            .and()
-//            .authorizeRequests()
-//                .requestMatchers("/public/**").permitAll() // Allow requests to "/public/**" without authentication
-//                .anyRequest().authenticated()
-//            .and()
-//            .httpBasic(); // Use HTTP Basic Authentication
-//    }
-//}
+package com.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig  {
+
+	
+	
+	@Bean
+	 public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(c->c.disable()) // Disable CSRF
+            .cors(c->c.disable())            // Enable CORS
+            
+            .authorizeHttpRequests(auth->auth
+                 // Allow requests to "/public/**" without authentication
+                .anyRequest().authenticated())
+            
+            .httpBasic(b->b.realmName("noor")); // Use HTTP Basic Authentication
+        
+        return http.build();
+    }
+}
